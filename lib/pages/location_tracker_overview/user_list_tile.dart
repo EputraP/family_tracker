3ac5028eb8b7b3aad3d560eb3eb2535b34cec32f
eputrap <eputrap@gmail.com>
2024-07-13 21:32:28 +0700

    20240713_user_list_tile_

--- lib/pages/location_tracker_overview/user_list_tile.dart
+++ lib/pages/location_tracker_overview/user_list_tile.dart
@@ -9,8 +9,68 @@ class UserListTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       child: Container(
-        width: 150,
+        width: 220,
         color: Colors.lightBlue,
+        child: Row(
+          children: [
+            Container(
+              width: 65,
+              // color: Colors.red,
+              child: Icon(
+                Icons.circle_sharp,
+                size: 30,
+                color: Colors.blue,
+              ),
+            ),
+            VerticalDivider(
+              width: 5,
+              indent: 10,
+              endIndent: 10,
+            ),
+            Expanded(
+                child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Eka",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    SizedBox(
+                      height: 3,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        height: 18,
+                        width: 60,
+                        color: Colors.grey,
+                        child: Center(
+                          child: Text(
+                            "Idle",
+                            style: TextStyle(fontSize: 12),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 3,
+                    ),
+                    Text(
+                      "Jl.oilenfoiwefoinweoifnowiefn",
+                      style: TextStyle(fontSize: 12),
+                      overflow: TextOverflow.ellipsis,
+                    )
+                  ],
+                ),
+              ),
+            ))
+          ],
+        ),
       ),
       onTap: () {
         Get.to(() => LocationTrackerDetails());
