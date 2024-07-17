68c831658a136491fe7b2bb31b0167f72f0f0cd8
eputrap <eputrap@gmail.com>
2024-07-18 03:37:01 +0700

    add mvc in every page

---
+++ lib/pages/location_tracker/location_tracker_details/controllers/location_overview_view_controllers.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LocationOverviewViewControllers extends StatelessWidget {
+  const LocationOverviewViewControllers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/location_tracker/location_tracker_details/views/dragable_bottom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DragableBottomSheet extends StatelessWidget {
+  const DragableBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
