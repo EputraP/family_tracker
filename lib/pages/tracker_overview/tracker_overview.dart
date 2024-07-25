80ed821b1159f2762cac090c109ed464e3bcf548
eputrap <eputrap@gmail.com>
2024-07-25 19:53:58 +0700

    separate fab circular widget

--- lib/pages/tracker_overview/tracker_overview.dart
+++ lib/pages/tracker_overview/tracker_overview.dart
@@ -1,8 +1,7 @@
 import 'package:fab_circular_menu_plus/fab_circular_menu_plus.dart';
 import 'package:family_tracker/constans/colors_collection.dart';
 import 'package:family_tracker/controllers/user_auth_controller.dart';
-import 'package:family_tracker/pages/login_page/views/login_page.dart';
-import 'package:family_tracker/pages/tracker_overview/views/circular_button.dart';
+import 'package:family_tracker/pages/tracker_overview/views/fab_circular_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -16,63 +15,7 @@ class TrackerOverview extends StatelessWidget {
       body: Center(
         child: Text("Family Tracker Coming Soon"),
       ),
-      floatingActionButton: Builder(
-          builder: (context) => FabCircularMenuPlus(
-                key: fabKey,
-                alignment: Alignment.bottomRight,
-                ringColor: Colors.transparent,
-                ringDiameter: 280.0,
-                ringWidth: 80.0,
-                fabSize: 60.0,
-                fabElevation: 8.0,
-                fabIconBorder: CircleBorder(),
-                fabColor: Colors.white,
-                fabOpenIcon: Icon(Icons.menu, color: Colors.black),
-                fabCloseIcon: Icon(Icons.close, color: Colors.blue),
-                fabMargin: const EdgeInsets.only(top: 50),
-                animationDuration: const Duration(milliseconds: 800),
-                animationCurve: Curves.easeInOutCirc,
-                children: <Widget>[
-                  CircularButton(
-                    color: Colors.black,
-                    width: 50,
-                    height: 50,
-                    icon: Icon(
-                      Icons.logout,
-                      color: Colors.white,
-                    ),
-                    onClick: () {
-                      userAuthController.changeIsAuthVal(false);
-                      userAuthController.emptyToken();
-                      Get.to(LoginPage());
-                    },
-                  ),
-                  CircularButton(
-                    color: Colors.black,
-                    width: 50,
-                    height: 50,
-                    icon: Icon(
-                      Icons.person,
-                      color: Colors.white,
-                    ),
-                    onClick: () {
-                      print('Second button');
-                    },
-                  ),
-                  CircularButton(
-                    color: Colors.black,
-                    width: 50,
-                    height: 50,
-                    icon: Icon(
-                      Icons.person_add,
-                      color: Colors.white,
-                    ),
-                    onClick: () {
-                      print('Second button');
-                    },
-                  ),
-                ],
-              )),
+      floatingActionButton: Builder(builder: (context) => FabCircularMenu()),
     );
   }
 }
