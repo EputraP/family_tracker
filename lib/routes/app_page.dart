471fb54094381cfa6872cc064c977ec3f7708413
Eka Putra Prasetya <eka.putra@caditglobal.com>
2024-07-26 09:40:04 +0800

    delete unused import package

--- lib/routes/app_page.dart
+++ lib/routes/app_page.dart
@@ -1,7 +1,6 @@
 import 'package:family_tracker/bindings/map_binding.dart';
 import 'package:family_tracker/pages/location_tracker/location_tracker_details/location_tracker_details.dart';
 import 'package:family_tracker/pages/location_tracker/location_tracker_overview/location_tracker_overview.dart';
-import 'package:family_tracker/pages/login_page/views/login_page.dart';
 import 'package:family_tracker/routes/app_url.dart';
 import 'package:get/get.dart';
 
